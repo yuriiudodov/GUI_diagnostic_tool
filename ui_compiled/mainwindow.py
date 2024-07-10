83fee0bb149ca970a1a3b23617fd9e9ded312fa1
yuriiudodov <udodov.ura2003@gmail.com>
2024-07-10 14:33:21 +0300

    diagnostic window

--- ui_compiled/mainwindow.py
+++ ui_compiled/mainwindow.py
@@ -2,14 +2,17 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 
+from main_menu_ui import Ui_Dialog
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
     sys.exit(app.exec())
